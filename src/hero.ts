32c991cf747b7e9ca83c5c7cce47a54ab4d75ce9
Sami Daly <samidaly19@outlook.com>
2026-01-05 11:37:58 +0100

    in pregress

---
+++ src/hero.ts
@@ -0,0 +1,9 @@
+export function products() {
+  const implementProducts = document.getElementById("products");
+
+  const heroProducts = ["./img/image 4.png", "./img/Vector.png"];
+
+  for (let i = 0; i < heroProducts.length; i++) {
+    const coffee = heroProducts[i];
+  }
+}
