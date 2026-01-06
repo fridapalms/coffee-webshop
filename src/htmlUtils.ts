bcaec5bc8bfa80ab982908123cbb281766a4d0ef
Frida <fridapalms@live.se>
2026-01-06 11:24:24 +0100

    started product page

---
+++ src/htmlUtils.ts
@@ -0,0 +1,42 @@
+import type { Product } from "./models/Product";
+
+//Funktion createHtml
+export const createHtml = (product: Product) => {
+  const productsdiv = document.getElementById("products");
+
+  //Skapa element
+  const productcard = document.createElement("div");
+  const imgdiv = document.createElement("div");
+  const img = document.createElement("img");
+  const infodiv = document.createElement("div");
+  const textdiv = document.createElement("div");
+  const producttitle = document.createElement("h3");
+  const productprice = document.createElement("h4");
+  const cartbtn = document.createElement("button");
+  const cartimg = document.createElement("img");
+
+  //Ändra element
+  productcard.className = "productcard";
+  imgdiv.className = "imgdiv";
+  img.src = product.heroimage;
+  infodiv.className = "infodiv";
+  textdiv.className = "textdiv";
+  producttitle.className = "producttitle";
+  producttitle.innerHTML = product.title;
+  productprice.className = "productprice";
+  productprice.innerHTML = product.price;
+  cartbtn.className = "cartbtn";
+  cartimg.className = "cartimg";
+  cartimg.src = product.carticon;
+
+  //Placera element
+  imgdiv.appendChild(img);
+  cartbtn.appendChild(cartimg);
+  infodiv.appendChild(textdiv);
+  infodiv.appendChild(cartbtn);
+  textdiv.appendChild(producttitle);
+  textdiv.appendChild(productprice);
+  productcard.appendChild(imgdiv);
+  productcard.appendChild(infodiv);
+  productsdiv?.appendChild(productcard);
+};
