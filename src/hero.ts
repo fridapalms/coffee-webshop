a1c10a4d8e217f221d9ded724159adccce016928
Sami Daly <samidaly19@outlook.com>
2026-01-06 12:35:33 +0100

    done

--- src/hero.ts
+++ src/hero.ts
@@ -3,10 +3,10 @@ export function products() {
   if (!container) return;
 
   const heroProducts = [
-    { image: "/public/img/image 4.png", icon: "/public/img/vector.png" },
-    { image: "/public/img/image 4.png", icon: "/public/img/vector.png" },
-    { image: "/public/img/image 4.png", icon: "/public/img/vector.png" },
-    { image: "/public/img/image 4.png", icon: "/public/img/vector.png" },
+    { image: "/public/img/image 4.png", icon: "/public/img/vector.png", name: "KAFFEBÖNOR\n299.00 kr" },
+    { image: "/public/img/image 4.png", icon: "/public/img/vector.png", name: "KAFFEPETTERS\n299.00 kr" },
+    { image: "/public/img/image 4.png", icon: "/public/img/vector.png", name: "KARLSSONS\n299.00 kr" },
+    { image: "/public/img/image 4.png", icon: "/public/img/vector.png", name: "KOFFEBRODERNS\n299.00 kr" },
   ];
 
   container.innerHTML = "";
@@ -18,7 +18,7 @@ export function products() {
 
     const img = document.createElement("img");
     img.src = product.image;
-    img.alt = "Kaffe Bonor";
+    img.alt = "coffeImg";
     img.className = "productImg";
 
     const icon = document.createElement("img");
@@ -26,8 +26,14 @@ export function products() {
     icon.alt = "varukorg";
     icon.className = "icon";
 
+    const text = document.createElement("p");
+    text.className = "coffeeText";
+    text.textContent = product.name;
+    text.style.whiteSpace = "pre-line";
+
     card.appendChild(img);
     card.appendChild(icon);
+    card.appendChild(text);
 
     container.appendChild(card);
   }
