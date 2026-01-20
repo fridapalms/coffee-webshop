1862d6774ff3e6cfdd21654bd7424807df569f9e
Ingrid <ingridstanic@gmail.com>
2026-01-20 11:51:45 +0100

    small changes to plus/minus btn scss

--- src/main.ts
+++ src/main.ts
@@ -13,61 +13,7 @@ import {
 
 //import { productInfo } from "./productInfo/productDetails";
 
-//Cookie
 cookiePopUp();
-
-// const cart: cartItem[] = [];
-
-// // localstorage
-// const CART_KEY = "cart";
-
-// function cartLocalStorage() {
-//   localStorage.setItem(CART_KEY, JSON.stringify(cart));
-// }
-
-// function loadCartLocalStorage() {
-//   const storedCart = localStorage.getItem(CART_KEY);
-//   if (!storedCart) return;
-
-//   try {
-//     const storedCartItems = JSON.parse(storedCart) as Array<{
-//       product: {
-//         title: string;
-//         weight: string;
-//         info: string;
-//         price: string;
-//         heroimage: string;
-//         secondimage: string;
-//         thirdimage: string;
-//         fourthimage: string;
-//         carticon: string;
-//         productlink: string;
-//       };
-//       quantity: number;
-//     }>;
-
-//     cart.length = 0;
-
-//     storedCartItems.forEach((item) => {
-//       const product = new Product(
-//         item.product.title,
-//         item.product.weight,
-//         item.product.info,
-//         item.product.price,
-//         item.product.heroimage,
-//         item.product.secondimage,
-//         item.product.thirdimage,
-//         item.product.fourthimage,
-//         item.product.carticon,
-//         item.product.productlink,
-//       );
-
-//       cart.push({ product, quantity: item.quantity });
-//     });
-//   } catch {
-//     localStorage.removeItem(CART_KEY);
-//   }
-// }
 
 loadCartLocalStorage();
 
